1829853021e52a595c8b527673461784924a03d0
dansilovsky <dan.silovsky@centrum.cz>
2014-10-05 19:50:43 +0100

    Helper::getRepositoryMock() now return Mockery\CompositeExpectation instead of \Mockery\MockInterface. So it returned object can receive more expectations.

--- tests/Helpers.php
+++ tests/Helpers.php
@@ -9,12 +9,12 @@ class Helpers {
 	 * Return traversable mock of selection which has one or more results (Nette\Database\Table\ActiverRow instances).
 	 * At each iteration returns \Tests\ActiveRowMock.
 	 * 
-	 *   eg. Helpers::getRepositoryMock(['id', 'username'], [[1, 'dan'], [2, 'petr'], [3, 'bea']]);
+	 *   eg. Helpers::getRepositoryMock(['id', 'username'], [[1, 'dan'], [2, 'petr'], [3, 'bea']])->getMock();
 	 * 
 	 * @param array $keys array of keys in selection
 	 * @param array $rows 
 	 * @param string $repositoryClass optional
-	 * @return \Mockery\MockInterface
+	 * @return \Mockery\CompositeExpectation
 	 */
 	static public function getRepositoryMock(array $keys, array $rows, $repositoryClass = 'Nette\Database\Table\Selection')
 	{
@@ -43,11 +43,12 @@ class Helpers {
 		$returnValidValues[] = false;
 		
 		return m::mock($repositoryClass)
+			->shouldReceive('getContext')->never()
 			->shouldReceive('rewind')
 			->shouldReceive('valid')->times($timesValid)->andReturnValues($returnValidValues)
 			->shouldReceive('current')->times($timesCurrent)->andReturnValues($returnCurrentValues)
 			->shouldReceive('next')
-			->getMock();
+			;
 	}
 }
 
